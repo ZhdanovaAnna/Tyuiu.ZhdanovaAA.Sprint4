b36ba4bae57afbe82687087010bc10d0121acfb5
ZhdanovaAnna <annaone01@mail.ru>
2023-11-04 01:16:48 +0500

    Task3

---
+++ Tyuiu.ZhdanovaAA.Sprint4.Task3.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhdanovaAA.Sprint4.Task3.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhdanovaAA.Sprint4.Task3.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78fb8230-3a97-45ba-87d3-d7057a0ff478")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZhdanovaAA.Sprint4.Task3.V25/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZhdanovaAA.Sprint4.Task3.V25.Lib;
+
+namespace Tyuiu.ZhdanovaAA.Sprint4.Task3.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[5, 5] { { 7, 3, 5, 3, 6 },
+                                     { 4, 6, 2, 5, 7 },
+                                     { 2, 3, 3, 3, 5 },
+                                     { 2, 7, 7, 6, 2 },
+                                     { 6, 6, 4, 3, 6 } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int colums = mtrx.Length / rows;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Жданова А. А. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+            Console.WriteLine("* Задание: 3                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнила: Жданова Анна Александровна                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 2 до 7. Найдите произведение     *");
+            Console.WriteLine("* элементов в четвертом столбце массива.                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            { 
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Произведение элементов в 4-ом столбце = {ds.Calculate(mtrx)}");
+            Console.ReadKey();
+        }
+    }
+}
