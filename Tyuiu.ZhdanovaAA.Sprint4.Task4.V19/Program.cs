303bc3a0649f4ab8ff4517437abf90f2522d0e0b
ZhdanovaAnna <annaone01@mail.ru>
2023-11-07 23:34:34 +0500

    Task4

---
+++ Tyuiu.ZhdanovaAA.Sprint4.Task4.V19/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZhdanovaAA.Sprint4.Task4.V19.Lib;
+
+namespace Tyuiu.ZhdanovaAA.Sprint4.Task4.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Жданова А. А. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание: 4                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнила: Жданова Анна Александровна                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 7. Найдите сумму нечетных   *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int colums = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, colums];
+
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"Введите {i},{j} элемент массива: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            { 
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Сумма нечётных элементов = {ds.Calculate(mtrx)}");
+            Console.ReadKey();
+        }
+    }
+}
