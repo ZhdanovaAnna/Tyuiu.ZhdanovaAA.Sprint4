89dc8958bba62cbdf7f3c82fcadb7bd40bee161b
ZhdanovaAnna <annaone01@mail.ru>
2023-11-10 00:16:56 +0500

    Task6

---
+++ Tyuiu.ZhdanovaAA.Sprint4.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZhdanovaAA.Sprint4.Task6.V4.Lib;
+
+namespace Tyuiu.ZhdanovaAA.Sprint4.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            var city = new string[] { "Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Самара" };
+            string[] res = ds.Calculate(city);
+            string[] wait = { "Москва", "Самара" };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
